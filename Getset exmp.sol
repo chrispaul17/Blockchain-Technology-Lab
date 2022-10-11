7a3cc1b8b328befed4d32a7ef17460d421e36749
chrispaul17 <57908563+chrispaul17@users.noreply.github.com>
2022-10-11 19:35:12 +0530

    Create Getset exmp.sol

---
+++ Getset exmp.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.0;
+contract MessageContract
+{
+    string message = "Hello World";
+    function getMessage() public view returns(string)
+    {return message;}
+    function setMessage(string newMessage) public
+    {message=newMessage;}
+}
