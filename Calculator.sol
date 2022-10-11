f5d45bcba55bbf794417aab7f31187b7b260e36a
chrispaul17 <57908563+chrispaul17@users.noreply.github.com>
2022-10-11 19:35:37 +0530

    Create Calculator.sol

---
+++ Calculator.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0;
+contract calculator 
+{
+    uint result;
+    function add(uint _a, uint _b) public {result = _a + _b;}
+    function sub(uint _a, uint _b) public {result = _a-_b;}
+    function mul(uint _a, uint _b) public {result = _a*_b;}
+    function div(uint _a, uint _b) public {result = _a/_b;}
+    function finalresult() view public returns(uint){return result;}
+}
